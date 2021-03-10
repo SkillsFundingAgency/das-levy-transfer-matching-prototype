c36fb7bccc3b9c01529fc6252e1f44fe4350b6df
James King <hello@jamesmichaelking.com>
2021-03-10 15:15:48 +0000

    Bullet points

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -86,7 +86,7 @@ function removeLocation() {
      $(this).closest(".govuk-form-group").remove();
 }
 
-var locationNumber = $("#locations").data("location-count");
+var locationNumber = $("#locations").data("location-count") || 1;
 $("#addAnotherLocation").on("click", addAnotherLocation);
 $("[data-remove]").on("click", removeLocation);
 
@@ -157,7 +157,7 @@ function removeEmail() {
      $(this).closest(".govuk-form-group").remove();
 }
 
-var emailNumber = $("#emails").data("email-count");
+var emailNumber = $("#emails").data("email-count") || 1;
 $("#addAnotherEmail").on("click", addAnotherEmail);
 $("[data-remove]").on("click", removeEmail);
 
