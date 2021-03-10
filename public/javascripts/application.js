8e58b66eca84efb739ecc370a2581f1db1e8520c
James King <hello@jamesmichaelking.com>
2021-03-10 12:00:24 +0000

    Location showing up on the check answers view

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -80,7 +80,6 @@ function addAnotherLocation() {
 }
 
 function removeLocation() {
-     event.preventDefault();
      var removeId = $(this).data("remove");
      $("#locations").append(`
           <input id="pledge-location[${removeId}]" name="pledge-location[${removeId}]" type="hidden" value="" />`
@@ -88,7 +87,7 @@ function removeLocation() {
      $(this).closest(".govuk-form-group").remove();
 }
 
-var locationNumber = $("#locations").data("location-count");
+var locationNumber = $("#locations").data("location-count") || 1;
 $("#addAnotherLocation").on("click", addAnotherLocation);
 $("[data-remove]").on("click", removeLocation);
 
@@ -132,7 +131,6 @@ $('input[name="pledge-level"]').on("click", function (e) {
 
 // Intermediaries
 function addAnotherEmail() {
-     event.preventDefault();
      emailNumber++;
      $("#emails").append(`
           <div class="govuk-form-group">
@@ -144,14 +142,12 @@ function addAnotherEmail() {
           </div>
      `).on("click", "[data-remove]", function () {
      if ($("#emails").data("email-count") === 1) {
-          event.preventDefault();
           $(this).closest(".govuk-form-group").remove();
      }
      });
 }
 
 function removeEmail() {
-     event.preventDefault();
      var removeId = $(this).data("remove");
      $("#emails").append(`
           <input class="govuk-input govuk-!-width-three-quarters" id="pledge-email[${removeId}]" name="pledge-email[${removeId}]" type="email" spellcheck="false">`
